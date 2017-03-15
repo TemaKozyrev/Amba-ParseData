d49c1ae559cfe6065a85b8b38a153c0b50bf50ea
TemaKozyrev <tema@tema.ws>
2017-03-15 12:07:47 +0300

    logger + async

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ var fs = require('fs');
 var jsonfile = require('jsonfile');
 var categoryFile = 'tmp/category.json';
 var categoryUrl = 'https://www.fasttech.net/support/ws/API.ashx?get=subcategories&CategoryID=3099';
-var category = require('./tmp/category.json');
 var async = require('async');
+var winston = require('winston');
 
 var parsePage = function (data, callback) {
-    // console.log(data.d.CategoryID + ': ' + data.d.CurrentPageIndex);
     if (data === undefined)
         callback({'error': 'undefined data'});
     else {
@@ -18,7 +17,7 @@ var parsePage = function (data, callback) {
         if (data.d.Success == false)
             callback({'error': 'bad request'});
         else {
-            console.log(data.d.CategoryID + ': ' + data.d.CurrentPageIndex);
+            winston.log('info', 'Category-' + data.d.CategoryID + ': Page-' + data.d.CurrentPageIndex);
             if (!fs.existsSync('./tmp/' + data.d.CategoryID)) {
                 fs.mkdirSync('./tmp/' + data.d.CategoryID);
             }
@@ -37,10 +36,11 @@ var parseCategory = function (data, callback) {
 
 
 
-function getCategory() {
+function getCategory(callback) {
     request(categoryUrl, function (error, response, body) {
         parseCategory(JSON.parse(body), function (category) {
-            jsonfile.writeFile(category.file, category.data)
+            jsonfile.writeFile(category.file, category.data);
+            callback(null, category.data)
         })
     })
 }
@@ -49,7 +49,7 @@ var getData = async.queue(function (task, callback) {
     request(task.url, function (err, res, body) {
         parsePage(body, function (resultPage) {
             if (resultPage.error)
-                console.log(resultPage.error + ': ' + tmpUrl);
+                winston.log('error', resultPage.error + ': ' + task.url);
             else
                 jsonfile.writeFile(resultPage.file, resultPage.data)
         });
@@ -57,8 +57,18 @@ var getData = async.queue(function (task, callback) {
     })
 }, 5);
 
-for (var i = 0; i < category.length; i++) {
-    for (var j = 0; j <= Math.floor(category[i].ProductCount / 20); j++) {
-        getData.push({url: 'https://www.fasttech.net/support/ws/API.ashx?get=products&CategoryID=' + category[i].ID + '&API=1&SKU=0&PageIndex=' + j + '&SearchKeywords=&Filters=&SortOption='})
+function makeTask(category, callback) {
+    for (var i = 0; i < category.length; i++) {
+        for (var j = 0; j <= Math.floor(category[i].ProductCount / 20); j++) {
+            getData.push({url: 'https://www.fasttech.net/support/ws/API.ashx?get=products&CategoryID=' + category[i].ID + '&API=1&SKU=0&PageIndex=' + j + '&SearchKeywords=&Filters=&SortOption='})
+        }
     }
+    callback(null);
 }
+
+async.waterfall([
+    getCategory,
+    makeTask
+], function (err, result) {
+
+});
