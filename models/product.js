947223672464bc87f0fc451cb1827c93a5a55405
TemaKozyrev <tema@tema.ws>
2017-03-18 13:11:01 +0300

    schemas

--- models/product.js
+++ models/product.js
@@ -1,3 +1,33 @@
 /**
  * Created by artemyel on 16.03.17.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var productSchema = new Schema({
+    SKU: Number,
+    name: String,
+    tagline: String,
+    optionName: String,
+    desc: [{value: String}],
+    filters: [{
+        _filterId: {
+            type: Schema.Types.ObjectId,
+            require: true,
+            ref: 'Filter'
+        },
+        value: String
+    }],
+    spec: [{type: String, value: String}],
+    relatedProducts: [{SKU: String}]
+
+});
+
+var categorySchema = new Schema({
+    name: String,
+    shortName: String,
+    products: [productSchema],
+    filters: [{name: String}]
+});
+
+module.exports = mongoose.model('Category', categorySchema);
